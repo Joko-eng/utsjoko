0558ff688d9a0cb536e24a5584f5d26b1f1c8117
Joko-eng <mohammadjoko1001@gmail.com>
2023-11-23 15:51:00 +0700

    ganti tampilan dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,7 @@
         rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- Feather Icons -->
     <script src="https://unpkg.com/feather-icons"></script>
 
@@ -40,11 +41,49 @@
     </header>
     
     <main>
-        <div class="image-with-text">
+        <div id="carouselExampleCaptions" class="carousel slide">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="assets/img/flower.jpg" class="d-block w-100" alt="gambar">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Brighten Your Day with Blooms</h5>
+                  <p>Our exquisite collection of flowers will mesmerize you with their vibrant colors and enchanting fragrances.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="assets/img/flow.jpg" class="d-block w-100" alt="gambar">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Express Emotions with Flowers</h5>
+                    <p>Let our thoughtfully crafted bouquets convey your feelings, whether it's love, gratitude, or sympathy.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="assets/img/flow1.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Explore the Beauty of Nature</h5>
+                    <p>Our exquisite collection of flowers will mesmerize you with their vibrant colors and enchanting fragrances.</p>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        {{-- <div class="image-with-text">
             <p>Temukan <br> Produk Khas <br>Kami Disini !</p>
             <a href="#" class="get-started-button">Selengkapnya</a>
             <img src="assets/img/flower.jpg" alt="Deskripsi Gambar Anda">
-        </div>
+        </div> --}}
         <section class="deskripsi-container">
             <div>
                 <div>
@@ -65,6 +104,6 @@
     <script>
         feather.replace()
     </script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,26 @@
 
 @section('content')
 <div class="container mt-5">
-    <div class="row justify-content-center">
+    <div class="row">
+        <!-- Sidebar -->
+        <div class="col-md-2">
+            <!-- Your sidebar content goes here -->
+            <div class="card">
+                <div class="card-body">
+                    <h6 class="card-title">Sidebar</h6>
+                    <!-- Add your sidebar content here -->
+                </div>
+            </div>
+        </div>
+
+        <!-- Main content -->
         <div class="col-md-8">
             <div class="card border-0 shadow-sm rounded">
                 <div class="card-body">
                     <h3 class="text-center my-4">Data produk</h3>
                     <hr>
                     <table class="table table-bordered">
-                    <a href="{{route('bunga.create')}} " class="btn btn-md btn-success mb-3">TAMBAH</a>
+                        <a href="{{ route('bunga.create') }}" class="btn btn-md btn-success mb-3">TAMBAH</a>
                         <thead>
                             <tr>
                                 <th scope="col">Nama</th>
@@ -26,17 +38,15 @@
                                 <td>{{ $bunga->stok }}</td>
                                 <td>{{ $bunga->harga }}</td>
                                 <td>
-                                        @if($bunga->image)
-                                        <img src="{{ asset('storage/images/' . $bunga->image) }}" alt="Gambar" style="max-width: 200px; max-height: 200px;">
-
-                                        @else
-                                        <p>Tidak ada gambar yang tersedia</p>
-                                        @endif
-                                    
-                                    </td>
+                                    @if($bunga->image)
+                                    <img src="{{ asset('storage/images/' . $bunga->image) }}" alt="Gambar" style="max-width: 200px; max-height: 200px;">
+                                    @else
+                                    <p>Tidak ada gambar yang tersedia</p>
+                                    @endif
+                                </td>
                                 <td class="text-center">
-                                        <form onsubmit="return confirm('Apakah Anda Yakin ?');" 
-                                            action="{{ route('bunga.destroy', $bunga->id) }}" method="POST">  
+                                    <form onsubmit="return confirm('Apakah Anda Yakin ?');" 
+                                        action="{{ route('bunga.destroy', $bunga->id) }}" method="POST">  
                                             <a href="{{ route('bunga.edit', $bunga->id) }}" {{ $bunga->id }} class="btn btn-sm btn-primary">Edit</a>
                                             @csrf
                                             @method('DELETE')
@@ -62,3 +72,5 @@
 </div>
 @endsection
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
